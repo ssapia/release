aa2660be2aa72dd3ab86f5666e3c9b7710bfd5a8
Salvador Sapia <salvador@ssapia.net>
2016-07-27 00:05:41 -0300

    Passa exibir a versão da dependencia

--- bin/release.sh
+++ bin/release.sh
@@ -35,19 +35,26 @@ run() {
 }
 
 releaseCmd() {
-   
-    showMsg "Releasing.... $1"
+  
+    projeto=$(echo ${1} | cut -d: -f1) 
+    versao=$(echo ${1} | cut -d: -f2)
+
+    if [ "${projeto}" == "${versao}" ]; then
+        showMsg "Releasing.... ${projeto}"
+    else 
+        showMsg "Releasing.... ${projeto}-${versao}"
+    fi
 
     if [ "$2" == "--list-only" ]; then
 	return
     fi
 
-    if [ ! -f "${PROJETOS}/${1}/pom.xml" ];then
-	showMsg "Erro, projeto '${1}' não encontrado!!!"
+    if [ ! -f "${PROJETOS}/${projeto}/pom.xml" ];then
+	showMsg "Erro, projeto '${projeto}' não encontrado!!!"
         exit 1
     fi
 
-    run "cd ${PROJETOS}/${1}"
+    run "cd ${PROJETOS}/${projeto}"
     run "git checkout master"
     run "git pull"
     run "mvn versions:use-releases -Dmessage='update from snapshot to release' 
@@ -59,7 +66,7 @@ release() {
   
     run "mvn dependency:list -f ${PROJETOS}/${1}/pom.xml > ${TMP_FILE}"
 
-    SNAPSHOTS=$(grep -- '-SNAPSHOT' ${TMP_FILE} | grep compile$ | cut -d: -f2 | tac)
+    SNAPSHOTS=$(grep -- '-SNAPSHOT' ${TMP_FILE} | grep compile$ | cut -d: -f2,4 | tac)
     
     for SNAPSHOT in ${SNAPSHOTS}; do
 	releaseCmd ${SNAPSHOT} ${2}
@@ -67,9 +74,6 @@ release() {
 
     releaseCmd ${PROJETO} ${2}
 }
-
-
-
 
 parametroInvalido() {
   echo "USO: $0 [OPCAO] [PROJETO]
